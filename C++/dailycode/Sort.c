0aa81309a251caa6bb711eafb044ae7ce4462914
chengq2018 <18168036523@168.com>
2019-08-16 23:54:10 +0800

    rewrite sort.c

--- C++/dailycode/Sort.c
+++ C++/dailycode/Sort.c
@@ -1,23 +1,113 @@
 #include "quick.h"
 
-int main()
+int quick_sort(int ary[], int ary_size)
 {
-    int test_ary[] = {3, 1, 2, 7, 15, 9, 11, 6, 4, 5};
-    //int test_ary[] = {1, 2, 3, 4, 5};
+    if (ary_size <= 1) {return 0;}
 
-    int ret = 0;
-    ret = quick_sort(test_ary, sizeof(test_ary)/sizeof(int));
+    int l = 0, r = ary_size - 1;
+    int hole = 0;
+    int tmp = ary[0]; //ary[0]作为基准值
 
-    //if (ret != 0)
-        //printf("fail\n");
-    //else
-
-    for (int i = 0; i < sizeof(test_ary)/sizeof(int); i++)
+    /*
+    * 右先挖个坑 ！
+    * 两种情况：
+    * 1. l = r 跳出：右找到左（右挖出的坑左侧的值比基准值大，即属于基准值的位置的土著比基准值大，右邻居比基准值小）
+    * 2. r < l 跳出：左找到右（左侧已全都小于基准值）
+    */
+    for ( ; r > l; r--)
     {
-        printf("ary[%d] = %d \n", i, test_ary[i]);
+        if (ary[r] < tmp)
+        {
+            ary[hole] = ary[r];
+            hole = r; // 右坑位记录
+
+            for ( ; l < r; l++) // 右侧挖出坑（右遍历查找到小于基准值得元素）后，左遍历
+            {
+                if (ary[l] > tmp)
+                {
+                    ary[hole] = ary[l];
+                    hole = l; // 左坑位记录
+                    break;
+                }
+            }
+
+            //防止r递减1
+            //if (r == l)
+                //break;
+        }
     }
 
+    if (l != r)
+    {
+        printf("error : l != r , l = %d, r = %d \n", l, r);
+        //return -1;
+    }
 
+    // 找到基准值的位置
+    ary[l] = tmp;
+    printf("l = %d, r = %d \n", l, r);
+
+    quick_sort(&ary[l + 1], ary_size - (l + 1));
+    quick_sort(&ary[0], l);
 
     return 0;
 }
+
+int i = 0 ;
+int tmp;
+void ExChange(int* a, int* b)
+{
+    tmp = *a;
+    //printf("tmp :%d", *tmp);
+    *a = *b;
+    *b =    tmp;
+}
+
+void Choose(int sortlst[], int n)
+{
+     for(; i<n-1; i++)
+    {
+        for(int k=i+1; k<n; k++)
+            if(sortlst[i]>sortlst[k])
+                (ExChange(&sortlst[i], &sortlst[k])) ;
+    }
+}
+
+void Insertlst(int sortlst[], int n)
+{
+    for(i=1; i<n; i++)
+    {
+        tmp = sortlst[i];
+        for(int k=i-1; k>=0; k--)
+           if(tmp<sortlst[k])
+            {
+                sortlst[k+1] = sortlst[k];
+                sortlst[k] = tmp;
+            }
+    }
+}
+
+void bubblesort(int sor[], int n)
+{
+    int i, j;
+    for(i=0; i<n-1; i++)
+    {
+        for(j=n-2; j>=i; j--)
+        {
+            (sor[j]>sor[j+1]) ? (ExChange(&sor[j], &sor[j+1])) : 0;
+        }
+    }
+}
+
+void bubblesorts(int sor[],int n)
+{
+    int i,j;
+    for(i = n-1;i>0;i--)
+    {
+        for(j=1;j<=i;j++)
+        {
+            (sor[j-1]>sor[j])?(ExChange(&sor[j-1],&sor[j])): 0;
+        }
+    }
+}
+
